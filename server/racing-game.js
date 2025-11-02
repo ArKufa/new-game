772e3f75975d757cefd379b225d942f252fc39ae
Mikiras <151003493+ArKufa@users.noreply.github.com>
2025-11-02 23:58:12 +0400

    Update racing-game.js

--- server/racing-game.js
+++ server/racing-game.js
@@ -1,10 +1,16 @@
 class RacingGame {
   constructor() {
     this.players = new Map();
+    this.tracks = {
+      desert: { length: 3000, color: '#D2691E', name: 'Пустыня' },
+      city: { length: 2500, color: '#708090', name: 'Город' },
+      mountain: { length: 3500, color: '#228B22', name: 'Горы' }
+    };
+    this.currentTrack = 'desert';
     this.gameState = {
       players: {},
       gameStarted: false,
-      trackLength: 2000
+      track: this.tracks.desert
     };
   }
 
@@ -14,14 +20,23 @@ class RacingGame {
     io.on('connection', (socket) => {
       console.log('Player connected:', socket.id);
 
-      socket.on('join-game', (playerName) => {
-        this.addPlayer(socket.id, playerName);
+      socket.on('join-game', (data) => {
+        this.addPlayer(socket.id, data.playerName, data.carType);
         socket.emit('game-state', this.gameState);
         socket.broadcast.emit('player-joined', {
           id: socket.id,
-          name: playerName,
+          name: data.playerName,
+          carType: data.carType,
           position: this.gameState.players[socket.id]
         });
+      });
+
+      socket.on('select-track', (trackName) => {
+        if (this.tracks[trackName]) {
+          this.currentTrack = trackName;
+          this.gameState.track = this.tracks[trackName];
+          this.io.emit('track-changed', this.gameState.track);
+        }
       });
 
       socket.on('player-move', (data) => {
@@ -38,21 +53,25 @@ class RacingGame {
       });
     });
 
-    // Game loop
     setInterval(() => {
       this.updateGame();
     }, 1000 / 60);
   }
 
-  addPlayer(id, name) {
+  addPlayer(id, name, carType) {
     this.gameState.players[id] = {
       id: id,
       name: name,
-      x: 100,
-      y: Object.keys(this.gameState.players).length * 80 + 100,
+      carType: carType || 'sports',
+      x: 0,
+      y: 0,
+      z: 0,
       speed: 0,
+      rotation: 0,
       progress: 0,
-      finished: false
+      finished: false,
+      lap: 1,
+      position: Object.keys(this.gameState.players).length
     };
   }
 
@@ -64,12 +83,26 @@ class RacingGame {
     const player = this.gameState.players[id];
     if (player && !player.finished) {
       player.speed = data.speed;
+      player.rotation = data.rotation;
+      player.x = data.x;
+      player.z = data.z;
       player.progress += player.speed;
       
-      if (player.progress >= this.gameState.trackLength) {
-        player.progress = this.gameState.trackLength;
+      if (player.progress >= this.gameState.track.length * 3) { // 3 круга
+        player.progress = this.gameState.track.length * 3;
         player.finished = true;
-        this.io.emit('player-finished', { id, position: player.progress });
+        this.io.emit('player-finished', { 
+          id, 
+          name: player.name,
+          position: player.progress 
+        });
+      } else if (player.progress >= this.gameState.track.length * player.lap) {
+        player.lap++;
+        this.io.emit('player-lap', { 
+          id, 
+          name: player.name,
+          lap: player.lap 
+        });
       }
     }
   }
